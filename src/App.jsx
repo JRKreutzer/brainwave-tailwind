4873a3deceb776888546837de9bbd4b000eef3e0
JRKreutzer <jose.kreutzer@gmail.com>
2024-03-11 17:53:36 -0300

    Sessão Hero finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello Word</h1>;
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+      <ButtonGradient />
     </>
   );
 };
